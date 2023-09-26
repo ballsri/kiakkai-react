aba359e3633589cffcecf5605605574a3cab88e8
ballsri <ball04407@hotmail.com>
2023-09-26 18:37:01 +0700

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { notificationProvider, RefineSnackbarProvider } from "@refinedev/mui";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import dataProvider, { GraphQLClient } from "@refinedev/graphql";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { authProvider } from "./authProvider";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+const API_URL = "https://your-graphql-url/graphql";
+
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <CssBaseline />
+          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+          <RefineSnackbarProvider>
+            <Refine
+              dataProvider={gqlDataProvider}
+              notificationProvider={notificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "Ty4wW7-OTiBOQ-OoMxBc",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+          </RefineSnackbarProvider>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
