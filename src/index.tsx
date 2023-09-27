ad8a919ee5b79b2fa06b6a8440218a8403979ae7
ballsri <ball04407@hotmail.com>
2023-09-28 00:25:01 +0700

    fix: deployment 1

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import config from "./aws-exports";
-import {Amplify} from "aws-amplify";
+import { Amplify } from "aws-amplify";
 
 Amplify.configure(config);
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
